4ab7e7a89aef9fb1667384db9a7d2b2cfcd0d0c5
el-dani-cortes <danicor83@gmail.com>
2021-08-04 14:51:59 -0500

    UPDATED | Added a condition to evaluate when malloc fail

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	heap_t *new_node = NULL;
 
 	new_node = malloc(sizeof(heap_t));
+	if (new_node == NULL)
+		return (NULL)
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
