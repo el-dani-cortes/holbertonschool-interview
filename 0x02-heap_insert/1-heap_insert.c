c1a2e485453a912c820a498da53ae155d2affc90
el-dani-cortes <danicor83@gmail.com>
2021-08-05 21:18:01 -0500

    UPDATED | Fixed betty format

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -5,10 +5,11 @@
 /**
  * swap_node - Swap nodes.
  *
- * @root: Pointer to the root of the tree
+ * @node: Pointer to the root of the tree
  * @value: New value to insert in the tree
- * 
- * Return: pointer to the node swaped, or NULL on failure 
+ * @side: Number to validate if left or right side
+ *
+ * Return: pointer to the node swaped, or NULL on failure
  */
 heap_t *swap_node(heap_t *node, int value, int side)
 {
@@ -35,7 +36,7 @@ heap_t *swap_node(heap_t *node, int value, int side)
 		}
 		swap_node(node, value, side);
 	}
-	return node;
+	return (node);
 }
 
 /**
@@ -43,43 +44,37 @@ heap_t *swap_node(heap_t *node, int value, int side)
  *
  * @root: Pointer to the root of the tree
  * @value: New value to insert in the tree
- * 
- * Return: pointer to the inserted node, or NULL on failure 
+ *
+ * Return: pointer to the inserted node, or NULL on failure
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	binary_tree_t *new_node = NULL, *inserted_node = NULL;
 
 	heap_t *head = *root;
-	// Check if the root node exists
+
 	if (root == NULL)
 		return (NULL);
-	
-	if (head == NULL) 
+
+	if (head == NULL)
 	{
 		new_node = binary_tree_node(head, value);
 		*root = new_node;
-		return *root;
+		return (*root);
 	}
-	//check if the node has to children and move to the next depending
 	if (head->left != NULL && head->right != NULL)
 	{
 		heap_insert(&head->left, value);
-	}// insert the node in the right position, according complete binary tree concept
+	}
 	else if (head->left != NULL)
 	{
 		new_node = binary_tree_node(head->right, value);
-		//swap and return pointer to the inserted node
 		inserted_node = swap_node(new_node, value, 1);
 	}
 	else
 	{
 		new_node = binary_tree_node(head->left, value);
-		//swap and return pointer to the inserted node
 		inserted_node = swap_node(new_node, value, 1);
 	}
 	return (inserted_node);
-
-	// compare value with the parent node's value
-	// If the the value is bigger than parent's value, swap nodes until the condition doesnt fit anymore
 }
