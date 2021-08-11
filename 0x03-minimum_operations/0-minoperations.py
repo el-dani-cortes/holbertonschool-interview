88d4bfd883b8c8da7f79592cbbc85593d5a09790
el-dani-cortes <danicor83@gmail.com>
2021-08-11 12:47:41 -0500

    ADDED  | Function to find the minimum operations to copy a letter until a given number

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to
+    result in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    num_operations = 0
+    while (n > 1):
+        for divisor in range(2, n + 1):
+            if (n % divisor == 0):
+                num_operations += divisor
+                n = n // divisor
+                break
+    return num_operations
