1d0b4a3dcfaccff1b6203430e2d108cf5021c56c
el-dani-cortes <danicor83@gmail.com>
2021-08-17 20:37:31 -0500

    ADDED | Solution for this task

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,22 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Function that computes the sum of two sandpiles*/
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+/*Print 3x3 grid*/
+void print_grid(int grid[3][3]);
+
+/*Print 3x3 grids sum*/
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+
+/*Copy a grid from another*/
+void copy_grip(int grid_result[3][3], int grid_sum[3][3]);
+
+/*Recursive toppling sandpiles round*/
+void toppling_round(int grid1[3][3]);
+
+#endif /* SANDPILES_H */
