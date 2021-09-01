755ec52ea49c2ccc9233971161890dbb07b4619e
el-dani-cortes <danicor83@gmail.com>
2021-09-01 12:14:45 -0500

    UPDATED | Changed the function to get the line and use EOFerror exception when is less than 10 lines

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -24,21 +24,27 @@ def get_data_from_stdin():
                    "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     order_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     file_size_total = 0
-    line_counts = 1
+    line_counts = 0
     try:
-        for line in sys.stdin:
-            check_pattern = re.search(log_pattern, line)
-            if check_pattern:
-                data = re.findall(r' (\d{3})\s(\d*)$', line)
-                if data[0][0] in status_code.keys():
-                    line_counts += 1
-                    status_code[data[0][0]] += 1
-                    file_size_total += int(data[0][1])
-                else:
-                    continue
-                if line_counts % 10 == 0 or line_counts != 1:
+        while True:
+            try:
+                line = input()
+                check_pattern = re.search(log_pattern, line)
+                if check_pattern:
+                    data = re.findall(r' (\d{3})\s(\d*)$', line)
+                    if data[0][0] in status_code.keys():
+                        line_counts += 1
+                        status_code[data[0][0]] += 1
+                        file_size_total += int(data[0][1])
+                    else:
+                        continue
+                    if line_counts >= 10:
+                        print_stats(file_size_total, status_code, order_codes)
+                        line_counts = 0
+            except EOFError:
+                if line_counts < 10:
                     print_stats(file_size_total, status_code, order_codes)
-                    line_counts = 1
+                break
     except KeyboardInterrupt:
         print_stats(file_size_total, status_code, order_codes)
 
